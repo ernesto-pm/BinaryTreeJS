0ca836fad1d2365f8058f233b542e06ac3edf77d
ErnestoPM <ernestopm20@gmail.com>
2017-01-30 20:32:34 -0600

    Aniadiendo nuevas pruebas

--- test/test.js
+++ test/test.js
@@ -16,17 +16,50 @@ describe('Array', function(){
 
     describe('crearArbol', function(){
         it('Debe crear un arbol con un valor en la raiz',function(){
-            var miArbol = new BinaryTree(5);
+            var miArbol = new BinaryTree();
+            miArbol.add(5);
             expect(miArbol.root).to.not.equal(null);
         })
     });
 
     describe('insertarNodoMenor', function(){
         it('Debe insertar un nodo con valor menor a la raiz del lado izquierdo',function(){
-            var miArbol = new BinaryTree(5);
-            miArbol.add(new Node(4));
-            expect(miArbol.left).to.not.equal(null);
+            var miArbol = new BinaryTree();
+            miArbol.add(5);
+            miArbol.add(4);
+            expect(miArbol.root.left).to.not.equal(null);
         })
     });
+
+    describe('insertarNodoMayor', function(){
+        it('Debe insertar un nodo con valor menor a la raiz del lado izquierdo',function(){
+            var miArbol = new BinaryTree();
+            miArbol.add(5);
+            miArbol.add(7);
+            expect(miArbol.root.right).to.not.equal(null);
+        })
+    });
+
+    describe('insertarNodoMenorAnidado', function () {
+        it('Debe insertar un nodo con valor menor al hijo izquierdo de la raiz', function () {
+            var miArbol = new BinaryTree();
+            miArbol.add(10);
+            miArbol.add(9);
+            miArbol.add(8);
+            expect(miArbol.root.left.left).to.not.equal(null);
+        })
+    })
+
+    describe('insertarNodoMenorAnidado', function () {
+        it('Debe insertar un nodo con valor mayor al hijo derecho de la raiz', function () {
+            var miArbol = new BinaryTree();
+            miArbol.add(10);
+            miArbol.add(11);
+            miArbol.add(12);
+            expect(miArbol.root.right.right).to.not.equal(null);
+        })
+    })
+
+
 });
 
